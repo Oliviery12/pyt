511b679b1dcc51af8bae6807e21f8d93487911bb
Oliviery12 <92150599+Oliviery12@users.noreply.github.com>
2022-01-23 21:32:35 +0100

    komit

--- test/test_main.py
+++ test/test_main.py
@@ -1,15 +1,15 @@
-
 import pytest
-
+import main
 def add(a,b):
     return a+b
 
 
 
 def test_add():
-    assert add(4,3) == 7
-    assert add(4,6) == 10
-    assert add(5,7) == 12
-    assert add(2,6) == 8
-    assert add(9,7) == 16
+    assert main.add(4,3) == 7
+    assert main.add(4,6) == 10
+    assert main.add(5,7) == 12
+    assert main.add(2,6) == 8
+    assert main.add(9,7) == 16
+
 
