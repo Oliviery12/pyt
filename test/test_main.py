8f4e4845ee908958cf5ea17ac278e34ca8fd4c27
Oliviery12 <92150599+Oliviery12@users.noreply.github.com>
2022-01-23 21:21:17 +0100

    jakis komit'
    jakis comit

--- test/test_main.py
+++ test/test_main.py
@@ -1,9 +1,12 @@
+
 import pytest
+import main
 def add(a,b):
     return a+b
 
 
 
 def test_add():
-    assert add(4,3) == 7
-    assert add(4,6) == 10
+    assert main.add(4,3) == 7
+    assert main.add(4,6) == 10
+    assert main.add(5,7) == 12
